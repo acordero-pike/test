6f1c8b098f7b4d17c8959328f4476c051c87d395
acordero-pike <76223645+acordero-pike@users.noreply.github.com>
2021-02-26 12:51:40 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const square = document.querySelectorAll('.square')
 const mole = document.querySelectorAll('.mole')
 const timeLeft = document.querySelector('#time-left')
 let score = document.querySelector('#score')
-
+ 
 let result = 0
 let currentTime = timeLeft.textContent
 
 function randomSquare() {
+
   square.forEach(className => {
     className.classList.remove('mole')
   })
@@ -17,12 +18,43 @@ function randomSquare() {
   hitPosition = randomPosition.id
 }
 
+
+function fondo(){
+  var sonido = document.createElement("iframe");
+	sonido.setAttribute("src","sounds/theme.mp3");
+	document.body.appendChild(sonido);
+	 	
+
+}
+
+function sonarPajaros(hitPosition){
+	var sonido = document.createElement("iframe");
+	sonido.setAttribute("src","sounds/smash.mp3");
+	document.body.appendChild(sonido);
+	document.getElementById(id.id).removeEventListener("click",sonarPajaros);
+  		
+
+}
+
+function callarPajaros(hitPosition){
+	var iframe = document.getElementsByTagName("iframe");
+
+	if (iframe.length > 0){
+		iframe[0].parentNode.removeChild(iframe[0]);
+		document.getElementById(id.id).addEventListener("click",sonarPajaros);
+	}
+}
+
+
 square.forEach(id => {
   id.addEventListener('mouseup', () => {
     if(id.id === hitPosition){
       result = result + 1
       score.textContent = result
+      document.getElementById("play").addEventListener("click",sonarPajaros(hitPosition));
+      document.getElementById("play").addEventListener("mouseup",callarPajaros(hitPosition));	
       hitPosition=null
+   
     }
   })
 })
@@ -31,16 +63,18 @@ square.forEach(id => {
 function moveMole() {
   let timerId = null
   timerId = setInterval(randomSquare, 600)
+ 
 }
 
 moveMole()
 
 
 function countDown() {
-  currentTime--
+  
   timeLeft.textContent = currentTime
-
+  currentTime--
   if(currentTime === 0 ) {
+     
     clearInterval(timerId)
          alert("Juego Finalizado el total de puntos fue de "+ result)
     alertify.confirm("Desea Repetir el juego ?.",
@@ -61,4 +95,5 @@ function countDown() {
 }
 
 let timerId = setInterval(countDown, 1000)
+
 
